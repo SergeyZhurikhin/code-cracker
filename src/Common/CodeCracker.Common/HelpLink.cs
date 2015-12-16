afe5dfbb636dff80782af8d397404ce32ba3d602
Сергей Журихин <forgsmtoday@mail.ru>
2015-12-16 19:49:27 +0300

    Configuration package VSIX, HelpLink

--- src/Common/CodeCracker.Common/HelpLink.cs
+++ src/Common/CodeCracker.Common/HelpLink.cs
@@ -2,7 +2,19 @@
 {
     public static class HelpLink
     {
-        public static string ForDiagnostic(DiagnosticId diagnosticId) =>
-            $"https://code-cracker.github.io/diagnostics/{diagnosticId.ToDiagnosticId()}.html";
+        private static readonly System.Collections.Generic.List<string> availableLocalizationCultureNames = new System.Collections.Generic.List<string> { "ru" };
+        public static string ForDiagnostic(DiagnosticId diagnosticId)
+        {
+            var name = System.Globalization.CultureInfo.CurrentCulture.Parent.Name;
+            if (availableLocalizationCultureNames.Contains(name))
+            {
+                return $"https://code-cracker.github.io/{name}/diagnostics/{diagnosticId.ToDiagnosticId()}.html";
+            }
+            else
+            {
+                return $"https://code-cracker.github.io/diagnostics/{diagnosticId.ToDiagnosticId()}.html";
+
+            }
+        }
     }
 }
